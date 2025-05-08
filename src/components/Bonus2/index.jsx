ed1403c860ed27d805fe9b45dc4fbc49afd008a5
Karolina Viragova <karolinavi94@gmail.com>
2025-05-08 17:50:31 +0200

    solution

--- src/components/Bonus2/index.jsx
+++ src/components/Bonus2/index.jsx
@@ -1,18 +1,21 @@
 import penizeUrl from './penize.svg';
 import './kasicka.css';
+import { useState } from 'react';
 
 // Zadání 1: Přichystejte v komponentě `Kasicka` proměnnou pro počet uložených peněz s výchozí hodnotou podle prop proměnné `pocatecniStav`.
 // Zadání 2: Prvním tlačítkem přidejte pět a druhým nastav na nulu.
 // Zadání 3: Počet peněz vypište.
 
 const Kasicka = ({ pocatecniStav }) => {
+  const [penizky, setPenizky] = useState(pocatecniStav);
+
   return (
     <div className="kasicka">
       <h3>
         <img src={penizeUrl} width={24} height={24} alt="" /> Peníze v kasičce:{' '}
-        0 Kč
+        {penizky} Kč
       </h3>
-      <button>přihodit pětikorunu</button> <button>vysypat</button>
+      <button onClick={() => {setPenizky(penizky + 5)}}>přihodit pětikorunu</button> <button onClick={() => {setPenizky(0)}}>vysypat</button>
     </div>
   );
 };
