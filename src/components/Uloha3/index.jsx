b2eb958c5ad5e6ad4f4885fe8d38104d2746a6ba
Karolina Viragova <karolinavi94@gmail.com>
2025-05-08 17:40:11 +0200

    splution

--- src/components/Uloha3/index.jsx
+++ src/components/Uloha3/index.jsx
@@ -2,11 +2,23 @@
 // Zadání 2: Proměnnou vypište místo `ano/ne/možná`.
 // Zadání 3: Po kliknutí na tlačítko změňte `možná` na `ano`, `ano` na `ne`, případně `ne` na `možná`.
 
+import { useState } from "react";
+
 export const Uloha3 = () => {
+  const [value, setValue] = useState("možná")
+
   return (
     <>
-      <h3>Prší v Brně: ano/ne/možná</h3>
-      <button>změnit</button>
+      <h3>Prší v Brně: {value}</h3>
+      <button onClick={() => {
+        if (value === "možná") {
+          setValue("ano")
+        } else if (value === "ano") {
+          setValue("ne")
+        } else {
+          setValue("možná")
+        }
+      }}>změnit</button>
     </>
   );
 };
