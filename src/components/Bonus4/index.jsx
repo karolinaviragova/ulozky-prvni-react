e75e72f1c17d59e6843a2f5159687542a8d81805
Karolina Viragova <karolinavi94@gmail.com>
2025-05-08 17:54:13 +0200

    solution

--- src/components/Bonus4/index.jsx
+++ src/components/Bonus4/index.jsx
@@ -1,10 +1,15 @@
 // Zadání: Pomocí dvou stavových proměnných s výchozí hodnotou 0, které se budou měnit podle vstupních políček, vypište do `<output></output>` jejich součet.
 
+import { useState } from "react";
+
 export const Bonus4 = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
   return (
     <>
-      <input type="number" defaultValue="0" /> +{' '}
-      <input type="number" defaultValue="0" /> = <output>0</output>
+      <input type="number" defaultValue="0" onChange={(e) => {setA(Number(e.target.value))}}/> +{' '}
+      <input type="number" defaultValue="0" onChange={(e) => {setB(Number(e.target.value))}}/> = <output>{a + b}</output>
     </>
   );
 };
